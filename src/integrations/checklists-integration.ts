52599f4140e81035eceeab19e9623fe6dcf7b333
Yeboster <marco@yeboster.com>
2023-08-27 19:19:26 +0200

    chore(deps): add rest client dep

--- src/integrations/checklists-integration.ts
+++ src/integrations/checklists-integration.ts
@@ -1,24 +1,25 @@
 import { TodoistClientType } from '../types'
 import IIntegration from '../integration'
+import { TodoistApi } from '@doist/todoist-api-typescript';
 
 export default class ChecklistIntegration implements IIntegration {
   get name() {
     return 'Checklists'
   }
 
-  async run(client: TodoistClientType) {
+  async run(syncClient: TodoistClientType, restClient: TodoistApi) {
     // Get parent checklists project
-    const checklistProject = client.projects!.get().find((project: { name: string; }) => project.name.toLowerCase() === 'checklists')
+    const checklistProject = syncClient.projects!.get().find((project: { name: string; }) => project.name.toLowerCase() === 'checklists')
 
     // Get all children projects under checklist project
-    const childProjects = client.projects!.get().filter((project: { parent_id: string; }) => project.parent_id === checklistProject.id)
+    const childProjects = syncClient.projects!.get().filter((project: { parent_id: string; }) => project.parent_id === checklistProject.id)
 
     // Get all tasks of checklist projects
-    const checklistTasks = childProjects.map((project: { id: string; }) => client.items!.get().filter((item: { project_id: string; }) => item.project_id === project.id)).flat()
+    const checklistTasks = childProjects.map((project: { id: string; }) => syncClient.items!.get().filter((item: { project_id: string; }) => item.project_id === project.id)).flat()
 
     // Remove due date from all tasks
     await Promise.all(checklistTasks.map(async (task: { id: string; }) => {
-      await client.items!.update({ id: task.id, due: null })
+      await syncClient.items!.update({ id: task.id, due: null })
     }))
   }
 }
