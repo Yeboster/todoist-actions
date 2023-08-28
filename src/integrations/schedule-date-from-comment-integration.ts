0c865d2dd8140930d06cecb67472a092641fa15f
Yeboster <marco@yeboster.com>
2023-08-28 10:03:49 +0200

    refactor: general improvements

--- src/integrations/schedule-date-from-comment-integration.ts
+++ src/integrations/schedule-date-from-comment-integration.ts
@@ -10,33 +10,23 @@ export default class ScheduleDateFromComment implements IIntegration {
   // Schedule tasks with due date and comments
   // Given a comment with the following date format (e.g. Sep 06 @ 1:15 PM)
   async run(syncClient: TodoistClientType, restClient: TodoistApi) {
-    const tasksWithDueDate = syncClient.items!.get().filter((item) => {
-      const hasDueDate = item.due && item.due.date
-      return hasDueDate
-    })
+    const tasksWithDateAndLabel = await restClient.getTasks({ filter: '!no date & !recurring & !no labels' })
 
-    const restTasks = await restClient.getTasks({ ids: tasksWithDueDate.map((task) => task.id) })
-
-    const tasksWithDueDateAndComment = restTasks.filter((task) => {
-      const hasComment = task.commentCount > 0
-      return hasComment
-    })
+    const tasksWithDueDateAndComment = tasksWithDateAndLabel.filter((task) => task.commentCount > 0)
 
     for (const task of tasksWithDueDateAndComment) {
       const comments = await restClient.getComments({ taskId: task.id })
 
       // Matches the following format Sep 06 @ 1:15 PM)
       const datePattern = /((\w|\s)+@\s.*(AM|PM))/g
-      const commentsWithDatePattern = comments.filter((comment) => {
-        return datePattern.test(comment.content)
-      })
+      const commentsWithDatePattern = comments.filter((comment) => datePattern.test(comment.content))
 
-      if (commentsWithDatePattern.length === 0) return
+      if (commentsWithDatePattern.length === 0) continue
 
       const commentWithDatePattern = commentsWithDatePattern[0]
 
       const datePatternMatch = commentWithDatePattern.content.match(datePattern)
-      if (datePatternMatch == null) return
+      if (datePatternMatch == null) continue
 
       const newDueDate = datePatternMatch[0]
 
